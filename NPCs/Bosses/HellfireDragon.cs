93f3b078881e45dc73c801a6c77ba4a28a559511
Noah Barsky <noahbarsky1@gmail.com>
2020-05-31 04:32:40 +0800

    Changed Comment

--- NPCs/Bosses/HellfireDragon.cs
+++ NPCs/Bosses/HellfireDragon.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Diagnostics;
 using System;
-//! note make robobee
+
 namespace Galactic.NPCs.Bosses {
     [AutoloadBossHead]
     public class HellfireDragon : ModNPC {
